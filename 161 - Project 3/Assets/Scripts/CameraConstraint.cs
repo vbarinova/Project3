01f5a3570beb28ad110f183079cb4f778b0c214d
aderevlany <avaderevlany@gmail.com>
2018-02-27 23:34:20 -0800

    Made simple movement system

---
+++ 161 - Project 3/Assets/Scripts/CameraConstraint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraConstraint : MonoBehaviour {
+
+	public float xMax; // stop camera at certain points
+	public float xMin;
+	public float yMax;
+	public float yMin;
+
+	private GameObject player;
+
+	private void Awake() {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {  // Calls at the end of an update
+		// Clamps the camera within the min and max positions
+		float x = Mathf.Clamp (player.transform.position.x, xMin, xMax);
+		float y = Mathf.Clamp (player.transform.position.y, yMin, yMax);
+
+		// Camera follows the player around up until the max and min values
+		gameObject.transform.position = new Vector3 (x, y, gameObject.transform.position.z); // dont wanna change the z, cuz its 2d
+
+	}
+}
---
+++ 161 - Project 3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+	public float m_Speed = 10.0f;
+
+	private float amountOfRotate = 10;
+	private Rigidbody2D m_rigidbody;
+
+	// Use this for initialization
+	void Awake () {
+		m_rigidbody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Movement ();
+	}
+
+	private void Movement()
+	{
+		// TODO: Replace GetKey() with GetAxis()
+		// W forward
+		if (Input.GetKey(KeyCode.W))
+			MoveFoward();
+		// S backwards
+		if (Input.GetKey(KeyCode.S))
+			MoveBackward();
+		// Left arrow rotate left
+		if (Input.GetKey ("right"))
+			Rotate (-amountOfRotate);
+		// Right arrow rotate right
+		if (Input.GetKey ("left"))
+			Rotate (amountOfRotate);
+	}
+
+	private void Rotate(float amount) {
+		// Rotate a set amount
+		transform.Rotate (new Vector3(0, 0, amount * m_Speed * Time.deltaTime));
+	}
+
+	private void MoveFoward() {
+		transform.position += transform.up * Time.deltaTime * m_Speed;
+	}
+
+	private void MoveBackward() {
+		transform.position -= transform.up * Time.deltaTime * m_Speed;
+	}
+}
