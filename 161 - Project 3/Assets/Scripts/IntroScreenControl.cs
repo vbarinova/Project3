90c2ac26360ea61e064a66999ef06fc74ee8da90
Victoria B <vbarinova97@gmail.com>
2018-03-06 17:08:45 -0800

    Main menu, starting screen, and temp music

---
+++ 161 - Project 3/Assets/Scripts/IntroScreenControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScreenControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.Slash)
+            || Input.GetKey(KeyCode.Slash) && Input.GetKey(KeyCode.Space)))
+        {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+}
