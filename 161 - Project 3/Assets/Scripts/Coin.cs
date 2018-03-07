6dbf1136460e0fa30c406f64716399104588b7c9
aderevlany <avaderevlany@gmail.com>
2018-03-06 16:50:30 -0800

    added collectables

---
+++ 161 - Project 3/Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	private float m_Speed = .8f;
+	private int amount = 90;
+	
+	// Update is called once per frame
+	void Update () {
+		turn ();
+	}
+
+	private void turn() {
+		transform.Rotate (new Vector3(0, amount * m_Speed * Time.deltaTime, 0));
+	}
+
+	private void OnTriggerEnter2D (Collider2D trig) {
+		// Debug.Log("Touch the end of the level!");
+		if (trig.gameObject.tag == "Player") {
+			Debug.Log ("+1 to collected peices, yay");
+			Destroy (gameObject);
+		}
+
+		}
+}
