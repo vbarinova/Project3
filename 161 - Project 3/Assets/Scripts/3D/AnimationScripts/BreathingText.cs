bc03f9bf139862746ce98ab01afe2fab6e00d7e8
aderevlany <avaderevlany@gmail.com>
2018-03-14 16:29:24 -0700

    Instrctions Screen

---
+++ 161 - Project 3/Assets/Scripts/3D/AnimationScripts/BreathingText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathingText : MonoBehaviour {
+
+	public float originalSize;
+	public float maxSize;
+	public float growFactor;
+	public float waitTime;
+
+	private void Awake() {
+		StartCoroutine (Scale ());
+	}
+
+	IEnumerator Scale() {
+		float timer = 0;
+		while (true) {
+			// Scale  to max value
+			while (maxSize > transform.localScale.x) {
+				timer += Time.deltaTime;
+				transform.localScale += new Vector3 (originalSize, originalSize, 0) * Time.deltaTime * growFactor;
+				yield return null;
+			}
+			// reset timer
+
+			yield return new WaitForSeconds (waitTime);
+
+			timer = 0;
+			while (originalSize < transform.localScale.x) {
+				timer += Time.deltaTime;
+				transform.localScale -= new Vector3 (originalSize, originalSize, 0) * Time.deltaTime * growFactor;
+				yield return null;
+			}
+
+			timer = 0;
+			yield return new WaitForSeconds (waitTime);
+		}
+	}
+		
+}
---
+++ 161 - Project 3/Assets/Scripts/InstructionsScenes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstructionsScenes : MonoBehaviour {
+
+    public GameObject SceneOne;
+    public GameObject SceneTwo;
+
+    private int changeScreen = 0;
+
+    // Use this for initialization
+    void Start () {
+        SceneOne.SetActive(true);
+        SceneTwo.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKey("space") && changeScreen == 0)
+        {
+            SceneOne.SetActive(false);
+            SceneTwo.SetActive(true);
+            Invoke("canChange", 0.5f);
+        }
+
+        else if (Input.GetKey("space") && changeScreen >= 1)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+    }
+
+    private void canChange()
+    {
+        ++changeScreen;
+        Debug.Log(changeScreen);
+    }
+}
