4a2ec424828c741597e3d2245c8ec42aebdb51d6
Victoria B <vbarinova97@gmail.com>
2018-03-14 22:30:16 -0700

    Sound buttons

---
+++ 161 - Project 3/Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour {
+
+	public static ButtonSound instance = null;
+	//public static ButtonSound buttonSound;
+	//public AudioClip buttonClick;
+
+	private AudioSource source;
+
+	// Use this for initialization
+	void Awake () {
+            source = GetComponent<AudioSource> ();
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+	
+	}
+	
+
+	public void Play() {
+        Debug.Log("Button sound plays!");
+		source.PlayOneShot (source.clip, 1f);
+	}
+}
