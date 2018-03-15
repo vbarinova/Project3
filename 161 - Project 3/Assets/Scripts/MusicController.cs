88a383b3c92a21702953fe3355d35022c4e425a0
Victoria B <vbarinova97@gmail.com>
2018-03-14 17:52:58 -0700

    Main Menu, Music

---
+++ 161 - Project 3/Assets/Scripts/MusicController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicController : MonoBehaviour
+{
+    public static MusicController instance = null;
+    public AudioSource music;
+
+    void Start()
+    {
+        if (instance != null)
+            { Destroy(gameObject); return; }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 3)
+            { Destroy(gameObject); return; }
+        else
+            if (!music.isPlaying) music.Play();
+    }
+
+}
